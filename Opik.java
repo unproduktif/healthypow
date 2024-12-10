4c91f4bc0e63c3456274d48c081b55e29cdd88f1
halid <you@example.com>
2024-12-10 16:42:35 +0800

    kokeor

--- Opik.java
+++ Opik.java
@@ -1,3 +1,9 @@
 public class Opik {
-    // ini saya lagi ngedit file opik java , diambna saya juga sanzgats  senang mengedti file ini 
+    // ini saya lagi ngedit file opik java , diambna saya juga sanzgats  senang mengedti file ini
+    
+    voif meki(){
+        System.out.println("kkk");
+    }
 }
+
+
